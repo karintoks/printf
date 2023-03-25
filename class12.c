16bf17cf59f02ef761519f22f7d9d862c1eb7813
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-03-25 16:08:37 -0700

    commit by OLOKUNTOYE TEMITOLA

---
+++ class12.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_percent - Prints a percent symbol
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ * This code is written by DONJOR KARINA and OLOKUNTOYE TEMITOLA
+ */
+int print_percent(__attribute__((unused))va_list list)
+{
+	_putchar('%');
+	return (1);
+}
