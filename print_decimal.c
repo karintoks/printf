ed882a495321cd17ba5d705a5b4c331ea26f3e83
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-03-25 13:46:38 -0700

    commit by OLOKUNTOYE TEMITOLA

---
+++ print_decimal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_decimal - prints a digit on the stdout
+ * @value: parameter passed
+ * Return: count
+ * This code is written by DONJOR KARINA and OLOKUNTOYE TEMITOLA
+ */
+int print_decimal(int value)
+{
+	int count = 0;
+
+	if (value >= 0)
+	{
+
+	}
+	return (count);
+}
