6fb0fdd899fa566858df97a5762ff62fb2f0cf19
karintoks <karintoks@gmail.com>
2023-03-29 00:20:36 -0700

    commit by DONJOR KARINA

--- case5-get_precision.c
+++ case5-get_precision.c
@@ -6,7 +6,7 @@
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
- * Return: Precision.
+ * Return: Precision..
  */
 int get_precision(const char *format, int *i, va_list list)
 {
