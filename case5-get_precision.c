0bef3fd622ea0e58ed30b91e2b0275a3f3ed4e60
karintoks <50800840+karintoks@users.noreply.github.com>
2023-06-25 07:02:49 +0100

    Update case5-get_precision.c

--- case5-get_precision.c
+++ case5-get_precision.c
@@ -3,34 +3,36 @@
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @i: Pointer to the index of the current character in the format string
+ * @list: List of arguments
  *
- * Return: Precision..
+ * Return: Precision
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-int curr_i = *i + 1;
-int precision = -1;
-if (format[curr_i] != '.')
-return (precision);
-precision = 0;
-for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-{
-if (is_digit(format[curr_i]))
-{
-precision *= 10;
-precision += format[curr_i] - '0';
-}
-else if (format[curr_i] == '*')
-{
-curr_i++;
-precision = va_arg(list, int);
-break;
-}
-else
-break;
-}
-*i = curr_i - 1;
-return (precision);
+	int curr_i = *i + 1;
+	int precision = -1;
+
+	if (format[curr_i] != '.')
+		return (precision);
+
+	precision = 0;
+	curr_i++;
+
+	if (format[curr_i] == '*')
+	{
+		curr_i++;
+		precision = va_arg(list, int);
+	}
+	else
+	{
+		while (is_digit(format[curr_i]))
+		{
+			precision = precision * 10 + (format[curr_i] - '0');
+			curr_i++;
+		}
+	}
+
+	*i = curr_i - 1;
+	return (precision);
 }
