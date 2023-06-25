9be588288333e1c6fa7ce8c82b56dadd271c82e6
karintoks <50800840+karintoks@users.noreply.github.com>
2023-06-25 07:31:22 +0100

    Update case5-get_precision.c

--- case5-get_precision.c
+++ case5-get_precision.c
@@ -11,13 +11,7 @@
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
-	int precision = -1;
-
-	if (format[curr_i] != '.')
-		return (precision);
-
-	precision = 0;
-	curr_i++;
+	int precision = 0;
 
 	if (format[curr_i] == '*')
 	{
