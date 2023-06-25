e1c6591e4ea98074013eccaefae9a09137035575
karintoks <50800840+karintoks@users.noreply.github.com>
2023-06-25 07:59:35 +0100

    Update case5-get_precision.c

--- case5-get_precision.c
+++ case5-get_precision.c
@@ -1,40 +1,112 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: Pointer to the index of the current character in the format string
- * @list: List of arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
- * Return: Precision
+ * Return: Precision..
  */
-int get_precision(const char *format, int *i, va_list list)
+int _printf(const char *format, ...)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+	va_list args;
+	int count = 0;
 
-	if (format[curr_i] != '.')
-		return (precision);
+	va_start(args, format);
+	count = handle_print(format, &args);
+	va_end(args);
 
-	precision = 0;
-	curr_i++;
+	return count;
+}
 
-	if (format[curr_i] == '*')
+/**
+ * handle_print - Handles the printing based on the format string
+ * @format: Format string
+ * @args: Variable arguments list
+ *
+ * Return: Number of characters printed
+ */
+int handle_print(const char *format, va_list *args)
+{
+	int count = 0;
+	int i = 0;
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return -1;
+
+	while (format[i])
 	{
-		curr_i++;
-		precision = va_arg(list, int);
-		if (precision < 0)
-			precision = -1;
-	}
-	else
-	{
-		while (is_digit(format[curr_i]))
+		if (format[i] == '%')
 		{
-			precision = precision * 10 + (format[curr_i] - '0');
-			curr_i++;
+			i++;
+			if (format[i] == '\0')
+				return -1;
+			if (format[i] == '%')
+			{
+				count += write_char('%');
+				i++;
+				continue;
+			}
+			count += handle_conversion(format[i], args);
+		}
+		else
+		{
+			count += write_char(format[i]);
+			i++;
 		}
 	}
 
-	*i = curr_i - 1;
-	return (precision);
+	return count;
+}
+
+/**
+ * handle_conversion - Handles the conversion specifier
+ * @specifier: Conversion specifier
+ * @args: Variable arguments list
+ *
+ * Return: Number of characters printed
+ */
+int handle_conversion(char specifier, va_list *args)
+{
+	if (specifier == 's')
+		return write_string(va_arg(*args, char *));
+	else if (specifier == 'c')
+		return write_char(va_arg(*args, int));
+	else if (specifier == '%')
+		return write_char('%');
+	else
+		return -1;
+}
+
+/**
+ * write_char - Writes a single character to stdout
+ * @c: Character to write
+ *
+ * Return: Number of characters printed (1 if successful, -1 otherwise)
+ */
+int write_char(char c)
+{
+	return write(1, &c, 1);
+}
+
+/**
+ * write_string - Writes a string to stdout
+ * @str: String to write
+ *
+ * Return: Number of characters printed
+ */
+int write_string(char *str)
+{
+	int len = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[len])
+		len += write_char(str[len]);
+
+	return len;
 }
