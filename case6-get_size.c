894593d99f0c879fdce3b3b6bdb55bf40c96d4b3
DoaaMouSSa <85126880+DoaaMouSSa@users.noreply.github.com>
2023-06-27 15:04:39 +0300

    Update case6-get_size.c

--- case6-get_size.c
+++ case6-get_size.c
@@ -12,7 +12,7 @@ int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
 	int size = 0;
-
+//check format
 	if (format[curr_i] == 'l')
 		size = S_LONG;
 	else if (format[curr_i] == 'h')
