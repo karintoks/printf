6212a3c9299d03ed17413404eac56cae80362675
DoaaMouSSa <85126880+DoaaMouSSa@users.noreply.github.com>
2023-06-27 15:03:55 +0300

    Update case4-get_flags.c

--- case4-get_flags.c
+++ case4-get_flags.c
@@ -16,7 +16,7 @@ int get_flags(const char *format, int *i)
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i = curr_i + 1)
 	{
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
 			if (format[curr_i] == FLAGS_CH[j])
