f3df80d899e7b10537dbe3407d2f3d7225cd21c4
karintoks <50800840+karintoks@users.noreply.github.com>
2023-06-28 01:54:05 +0100

    Update case8-handle_print.c

--- case8-handle_print.c
+++ case8-handle_print.c
@@ -23,7 +23,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+	for (i = 0; fmt_types[i].fmt != '\0'; i = i + 1)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
