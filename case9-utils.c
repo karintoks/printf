39b97f2a19169e368fbfa4a3ac9c7268681ce712
karintoks <50800840+karintoks@users.noreply.github.com>
2023-06-28 01:49:42 +0100

    commit by DONJOR KARINA

--- case9-utils.c
+++ case9-utils.c
@@ -26,7 +26,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
-		ascii_code *= -1;
+		ascii_code = ascii_code - 1;
 
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
